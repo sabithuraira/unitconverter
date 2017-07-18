c22384d01ae6280d99682fdc4c097a2a2ebc085e
Sabit Huraira <sabitzhabit@gmail.com>
2017-07-18 10:48:01 +0700

    initialize

---
+++ js/app.js
@@ -0,0 +1,56 @@
+(function() {
+	/**
+	 * Back key event handler
+	 */
+	window.addEventListener('tizenhwkey', function(ev) {
+		if (ev.keyName === "back") {
+			var activePopup = document.querySelector('.ui-popup-active'),
+				page = document.getElementsByClassName('ui-page-active')[0],
+				pageid = page ? page.id : "";
+
+			if (pageid === "main" && !activePopup) {
+				try {
+					tizen.application.getCurrentApplication().exit();
+				} catch (ignore) {
+				}
+			} else {
+				window.history.back();
+			}
+		}
+	});
+}());
+
+
+var app = {
+    init: function init() {
+        'use strict';
+
+        length.init();
+    },
+
+    /**
+     * Closes application.
+     *
+     * @public
+     */
+    exit: function exit() {
+        'use strict';
+
+        try {
+            tizen.application.getCurrentApplication().exit();
+        } catch (error) {
+            console.error(error);
+        }
+    },
+
+    /**
+     * Handles Low Battery event.
+     *
+     * @private
+     */
+//    onLowBattery: function onLowBattery() {
+//        'use strict';
+//
+//        this.exit();
+//    }
+};
---
+++ js/length.js
@@ -0,0 +1,59 @@
+
+var length = {
+
+    init: function init() {
+        'use strict';
+        	
+        this.selectedUnit=0;
+        this.enteredValue=1;
+    	this.datas=["mm", "cm", "dm", "m", "dam", "hm", "km"];
+        
+        this.form_length=document.getElementById('form_length');
+        this.dropdown_length=document.getElementById("dropdown_length");
+        this.body_length=document.getElementById("body_length");
+        
+        this.generateView();
+        this.bindEvents();
+    },
+    bindEvents: function bindEvents() {
+        'use strict';
+
+        var form_length=document.getElementById('form_length');
+        var dropdown_length=document.getElementById("dropdown_length");
+        var self = this;
+
+        dropdown_length.addEventListener('change',function onChange(){
+        	self.selectedUnit = this.selectedIndex;
+        	self.refreshResult();
+        });
+        
+        form_length.addEventListener('change', function onChange(){
+        	self.enteredValue = this.value;
+        	self.refreshResult();
+        });
+    },
+    generateView: function generateView(){
+    	
+    	for(var i=0;i<this.datas.length;++i){
+    		var opt = document.createElement('option');
+    	    opt.value = i;
+    	    opt.innerHTML = this.datas[i];
+    	    this.dropdown_length.appendChild(opt);
+    	    
+    	    var li_form=document.createElement('li');
+    	    li_form.innerHTML = this.datas[i]+"<span class='li-text-sub' id='result_"+i+"'>"+(1*Math.pow(10,i)).toLocaleString()+"</span>";
+    	    li_form.setAttribute("class", "ui-li-static");
+    	    this.body_length.appendChild(li_form);
+    	}
+    	
+    },
+    refreshResult: function refreshResult(){
+    	'use strict';
+    	
+    	for(var i=0;i< this.datas.length;++i){
+    	   var row_unit=document.getElementById('result_'+i);
+    	   row_unit.innerHTML = this.enteredValue * Math.pow(10, (i - this.selectedUnit));
+    	}
+    },
+    
+};
